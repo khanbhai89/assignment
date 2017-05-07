1c6ff428ce2475af5b31ffe41113fc082ea5bbd9
Hammad Ahmed <Hammad@administrators-MacBook-Pro.local>
2017-05-08 02:18:08 +0400

    All the changes suggested by Serhat

--- src/test/java/FacebookLogin/HappyPath.java
+++ src/test/java/FacebookLogin/HappyPath.java
@@ -1,89 +1,32 @@
 package FacebookLogin;
 
-import java.io.File;
-import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.*;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.annotations.AfterClass;
 import org.testng.annotations.Test;
-import org.testng.annotations.BeforeClass;
 import org.testng.Assert;
-import org.apache.commons.io.FileUtils;
 import com.relevantcodes.extentreports.ExtentReports;
-import com.relevantcodes.extentreports.ExtentTest;
 import com.relevantcodes.extentreports.LogStatus;
 
-public class HappyPath {
+import commonConfig.Config;
 
-	private WebDriver driver;
-	private String baseUrl;
-	private boolean acceptNextAlert = true;
-	private StringBuffer verificationErrors = new StringBuffer();
-	private ExtentReports report;
-	private ExtentTest test;
-
-	@BeforeClass
-	public void beforeClass() {
-		driver = new ChromeDriver();
-		baseUrl = "https://www.mamasandpapas.ae/";
-		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-		report = new ExtentReports("test-output/FacebookReport.html", true);
-	}
-
-	@AfterClass
-	public void afterClass() {
-		//driver.quit();
-		report.endTest(test);
-		report.flush();
-		
-		String verificationErrorString = verificationErrors.toString();
-		if (!"".equals(verificationErrorString)) {
-			Assert.fail(verificationErrorString);
-		}
-	}
+public class HappyPath extends Config {
 
 	@Test
-	public void testLogin(){
-		driver.get(baseUrl + "/");
+	public void testLogin() {
 		
+		report = new ExtentReports("test-output/FacebookReport.html", true);
 		test = report.startTest("Login to Mamas & Papas through Facebook.");
-		test.log(LogStatus.INFO, "Site Loaded");
-		TakeScreenShot ("siteloaded");
-
-		if (isAlertPresent()) {
-			closeAlertAndGetItsText();
-		}
-
-		if (isElementPresent(By.cssSelector("#onesignal-popover-container"))) {
-
-			TakeScreenShot ("pop-over");
-			test.log(LogStatus.INFO, "Pop-up for promotions is displayed.");
-			driver.findElement(By.cssSelector("#onesignal-popover-cancel-button")).click();
-			test.log(LogStatus.INFO, "Pop-up for promotions is closed.");
-		}
-
-		if (isElementPresent(By.cssSelector("#campaign-modal > div:nth-child(1) > div:nth-child(1)"))) {
-
-			TakeScreenShot ("modal-pop-up");
-
-			driver.findElement(By.cssSelector(
-					"#campaign-modal > div.vertical-alignment-helper > div.modal-dialog.vertical-align-center > div.modal-content > div.modal-header > button.close"))
-					.click();
-			test.log(LogStatus.INFO, "Modal Pop-up for promotions is closed.");
-		}
-
+		takeScreenShot ("siteloaded");
+		
 		driver.findElement(By.linkText("Sign In / Register")).click();
-		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 		driver.findElement(By.linkText("Facebook Connect")).click();
 
 		WebDriverWait wait = new WebDriverWait(driver, 40);
 		wait.until(ExpectedConditions.elementToBeClickable(By.id("email")));
 
 		driver.manage().timeouts().pageLoadTimeout(100, TimeUnit.SECONDS);
-		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 		
 		test.log(LogStatus.INFO, "Transfered to Facebook.");
 		
@@ -98,73 +41,29 @@ public class HappyPath {
 			driver.findElement(By.cssSelector("#user-info-box > span:nth-child(1)")).click();
 			test.log(LogStatus.PASS, "User is logged in through Facebook.");
 			Assert.assertTrue(isElementPresent(By.cssSelector("#user-info-box")));
-			TakeScreenShot ("Log-out");
+			takeScreenShot ("Log-out");
 			test.log(LogStatus.INFO, "Logging Out");
 			driver.findElement(By.id("logout-link")).click();
 		}
 		else {
 			
-			TakeScreenShot ("Not-Logged-in");
+			takeScreenShot ("Not-Logged-in");
 			test.log(LogStatus.FAIL, "User is not logged in through Facebook.");
 			Assert.assertFalse(!isElementPresent(By.cssSelector("#user-info-box")));
 		}
 		
 		if (isElementPresent(By.cssSelector("#welcome-and-cart > a.login-button"))) {
 			
-			TakeScreenShot ("Logged-Out");
+			takeScreenShot ("Logged-Out");
 			test.log(LogStatus.PASS, "User is logged out through Facebook.");
 			Assert.assertTrue(isElementPresent(By.cssSelector("#welcome-and-cart > a.login-button")));
 		}
 		else {
-			TakeScreenShot ("Not-Logged-Out");
+			takeScreenShot ("Not-Logged-Out");
 			test.log(LogStatus.FAIL, "User is not logged out through Facebook.");
 			Assert.assertFalse(!isElementPresent(By.cssSelector("#welcome-and-cart > a.login-button")));
 		}
 		
 	}
 	
-	private void TakeScreenShot (String name) {
-		
-		test.log(LogStatus.INFO, "Taking Screenshot");
-		//File screenShot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
-		//try {
-		//	FileUtils.copyFile(screenShot, new File("screenshots/" + name + ".png"));
-		//} catch (IOException e) {
-		//	// TODO Auto-generated catch block
-		//	e.printStackTrace();
-		//}
-	}
-
-	private boolean isElementPresent(By by) {
-		try {
-			driver.findElement(by);
-			return true;
-		} catch (NoSuchElementException e) {
-			return false;
-		}
-	}
-
-	private boolean isAlertPresent() {
-		try {
-			driver.switchTo().alert();
-			return true;
-		} catch (NoAlertPresentException e) {
-			return false;
-		}
-	}
-
-	private String closeAlertAndGetItsText() {
-		try {
-			Alert alert = driver.switchTo().alert();
-			String alertText = alert.getText();
-			if (acceptNextAlert) {
-				alert.accept();
-			} else {
-				alert.dismiss();
-			}
-			return alertText;
-		} finally {
-			acceptNextAlert = true;
-		}
-	}
 }
--- src/test/java/Search/HappyPath.java
+++ src/test/java/Search/HappyPath.java
@@ -1,171 +1,91 @@
 package Search;
 
-import java.io.File;
-import java.io.IOException;
-import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.*;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.annotations.AfterClass;
 import org.testng.annotations.Test;
-
+import org.testng.Assert;
 import com.relevantcodes.extentreports.ExtentReports;
-import com.relevantcodes.extentreports.ExtentTest;
 import com.relevantcodes.extentreports.LogStatus;
 
-import org.testng.annotations.BeforeClass;
-import org.testng.Assert;
-import org.apache.commons.io.FileUtils;
+import commonConfig.Config;
 
-public class HappyPath {
-	private WebDriver driver;
-	private String baseUrl;
-	private String searhText;
-	private boolean acceptNextAlert = true;
-	private StringBuffer verificationErrors = new StringBuffer();
-	private ExtentReports report;
-	private ExtentTest test;
-
-	@BeforeClass(alwaysRun = true)
-	public void setUp() {
-		driver = new ChromeDriver();
-		baseUrl = "https://www.mamasandpapas.ae/";
-		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-		
-		//report = new ExtentReports("test-output/SearchReport.html", true);
-		
-		driver.get(baseUrl + "/");
-		
-		//TakeScreenShot ("siteloaded");
-		
-		if (isAlertPresent()) {
-			closeAlertAndGetItsText();
-		}
-
-		if (isElementPresent(By.cssSelector("#onesignal-popover-container"))) {
-
-			driver.findElement(By.cssSelector("#onesignal-popover-cancel-button")).click();
-		}     
-		
-
-		if (isElementPresent(By.cssSelector("#campaign-modal"))) {
-			driver.findElement(By.cssSelector(
-					"#campaign-modal > div > div > div > div.modal-header > button"))
-					.click();
-		}
-	}
+public class HappyPath extends Config {
 
 	@Test
-	public void testSearchExist() throws IOException {
-
+	public void testSearchExist() {
+		
+		report = new ExtentReports("test-output/SearchReport.html", true);
+		test = report.startTest("Search Text Exists.");
 		searhText = "red";
 		
 		driver.findElement(By.name("q")).clear();
 		driver.findElement(By.name("q")).sendKeys(searhText);
+		test.log(LogStatus.INFO, "Searching for text \"red\".");
 		
 		WebDriverWait wait = new WebDriverWait(driver, 40);
-		wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("button.show-all")));
+		wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector(conf.getString("showAllButton"))));
 		
 		Assert.assertTrue(isElementPresent(By.cssSelector("button.show-all")));
 		
+		if (isElementPresent(By.cssSelector(conf.getString("showAllButton")))) {
+			
+			test.log(LogStatus.PASS, "Button show all exists.");
+		}
+		else {
+			takeScreenShot ("showalldontexist");
+			test.log(LogStatus.FAIL, "Button show all doesn't exist.");
+		}
 		
 		driver.findElement(By.name("q")).clear();
 		driver.findElement(By.name("q")).sendKeys(searhText);
-		driver.findElement(By.cssSelector("button.show-all")).click();
+		driver.findElement(By.cssSelector(conf.getString("showAllButton"))).click();
+		test.log(LogStatus.INFO, "Searching for text \"red\".");
 		String items = driver.findElement(By.cssSelector(".count > span:nth-child(1)")).getAttribute("innerHTML");
 		
 		String[] array1 = items.split("/");
 		
-		int ItemsOnPage = Integer.parseInt(array1[0]);
-		int TotalItems = Integer.parseInt(array1[0]);
+		int itemsOnPage = Integer.parseInt(array1[0]);
+		int totalItems = Integer.parseInt(array1[0]);
 
-		if (ItemsOnPage > 12 && ItemsOnPage > TotalItems) {
+		if (itemsOnPage > 12 && itemsOnPage > totalItems) {
 			Assert.assertTrue(isElementPresent(By.className("btn btn-empty load-more")));
+			test.log(LogStatus.PASS, "Load more button exists.");
 		}
 		else {
 			Assert.assertFalse(isElementPresent(By.className("btn btn-empty load-more")));
+			takeScreenShot ("loadmoredontexist");
+			test.log(LogStatus.FAIL, "Load more button doesn't exist.");
 		}
+		
+		report.endTest(test);
 		
 	}
 	
 	@Test
-	public void testSearchdDontExist() throws IOException {
+	public void testSearchdDontExist() {
 		driver.get(baseUrl + "/");
+		
+		test = report.startTest("Search Text Dont Exists.");
 		
 		searhText = "noitemswiththiskeyword";
 		
 		driver.findElement(By.name("q")).clear();
 		driver.findElement(By.name("q")).sendKeys(searhText);
+		driver.findElement(By.cssSelector("button.search")).click();
+		test.log(LogStatus.INFO, "Searching for text \"noitemswiththiskeyword\".");
 		
-		//Assert.assertFalse(isElementPresent(By.cssSelector("button.show-all")));
+		String resultMessage = driver.findElement(By.cssSelector(".blank-search-message")).getAttribute("innerHTML");
+		String expectedResultMessage = "You searched for <strong>"+ searhText +"</strong>, we're sorry we couldn't find anything to match your search.";
+		Assert.assertEquals(resultMessage, expectedResultMessage);
 		
+		if (resultMessage.equals(expectedResultMessage)) {
+			test.log(LogStatus.PASS, "Correct text is displayed.");
+		}
+		else {
+			test.log(LogStatus.PASS, "Incorrect text is displayed.");
+		}
 		
-		driver.findElement(By.name("q")).clear();
-		driver.findElement(By.name("q")).sendKeys(searhText);
-		driver.findElement(By.cssSelector("div.input-field > input[type=\"submit\"]:nth-child(2)")).click();
-		String ResultMessage = driver.findElement(By.cssSelector(".blank-search-message")).getAttribute("innerHTML");
-		
-		String ExpectedResultMessage = "You searched for <strong>"+ searhText +"</strong>, we're sorry we couldn't find anything to match your search.";
-		Assert.assertEquals(ResultMessage, ExpectedResultMessage);
-		
-		//Assert.assertFalse(isElementPresent(By.className("btn btn-empty load-more")));
 	}
 	
-	private void TakeScreenShot (String name) {
-		
-		test.log(LogStatus.INFO, "Taking Screenshot");
-		File screenShot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
-		try {
-			FileUtils.copyFile(screenShot, new File("screenshots/" + name + ".png"));
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
-
-	@AfterClass(alwaysRun = true)
-	public void tearDown() throws Exception {
-		//driver.quit();
-		report.endTest(test);
-		report.flush();
-		
-		String verificationErrorString = verificationErrors.toString();
-		if (!"".equals(verificationErrorString)) {
-			Assert.fail(verificationErrorString);
-		}
-	}
-
-	private boolean isElementPresent(By by) {
-		try {
-			driver.findElement(by);
-			return true;
-		} catch (NoSuchElementException e) {
-			return false;
-		}
-	}
-
-	private boolean isAlertPresent() {
-		try {
-			driver.switchTo().alert();
-			return true;
-		} catch (NoAlertPresentException e) {
-			return false;
-		}
-	}
-
-	private String closeAlertAndGetItsText() {
-		try {
-			Alert alert = driver.switchTo().alert();
-			String alertText = alert.getText();
-			if (acceptNextAlert) {
-				alert.accept();
-			} else {
-				alert.dismiss();
-			}
-			return alertText;
-		} finally {
-			acceptNextAlert = true;
-		}
-	}
 }
